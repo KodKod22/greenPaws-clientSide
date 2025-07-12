4932b6c1c80db49705596fd8c90eacf87fe32625
Kody agin peters <131360013+KodKod22@users.noreply.github.com>
2025-07-12 13:50:13 +0300

    connecting the updating report to the server side

--- js/reportPage.js
+++ js/reportPage.js
@@ -67,11 +67,109 @@ function setCardData(key,reportCard,cardData,product){
             case "requestid":
                 reportCard.setAttribute("Report_ID", product[key]);
                 break;
+            case "username":
+                cardData = document.createElement("span");
+                cardData.innerText = "User: " + product[key];
+                reportCard.appendChild(cardData);
+                break;
             default:
                 break; 
         }
 }
+async function sendAdminResponse(requestId,updatedResponse,updatedStatus){
+    console.log(requestId);
+    try {
+            const res = await fetch(`http://localhost:8081/api/requests/updateRequest`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    requestId: requestId,
+                    status: updatedStatus,
+                    adminResponds: updatedResponse
+                })
+            });
+
+            if (!res.ok) {
+                const err = await res.json();
+                throw new Error(err.error || "Update failed");
+            }
+
+            alert("Update saved successfully.");
+            window.location.reload();
+        } catch (error) {
+            console.error("Update error:", error.message);
+            alert("Failed to update report: " + error.message);
+        }
+}
+function setReportModel(userType,product,requestId){
+     const modalBody = document.getElementById("reportModalBody");
+
+        modalBody.innerHTML = "";
+
+        const description = document.createElement("p");
+        description.innerText = "Report description:" + (product.description || "No description");
+         modalBody.appendChild(description);
+        if (userType === "admin") {
+            const responseLabel = document.createElement("label");
+            responseLabel.innerText = "Manager response:";
+            const responseInput = document.createElement("textarea");
+            responseInput.id = "adminResponseInput";
+            responseInput.classList.add("form-control");
+            responseInput.rows = 4;
+            responseInput.placeholder = "Enter your response here...";
+            responseInput.value = product.adminResponds || "";
+
+        // שדה סטטוס
+            const statusLabel = document.createElement("label");
+            statusLabel.classList.add("mt-3");
+            statusLabel.innerText = "Change status:";
+            const statusSelect = document.createElement("select");
+            statusSelect.id = "statusSelect";
+            statusSelect.classList.add("form-select");
+
+            const statuses = ['new','close','progressive'];
+            statuses.forEach(status => {
+                const option = document.createElement("option");
+                option.value = status;
+                option.innerText = status.charAt(0).toUpperCase() + status.slice(1);
+                if (product.status === status) {
+                    option.selected = true;
+                }
+                statusSelect.appendChild(option);
+            });
+
+        modalBody.appendChild(responseLabel);
+        modalBody.appendChild(responseInput);
+        modalBody.appendChild(statusLabel);
+        modalBody.appendChild(statusSelect);
+
+        const saveButton = document.createElement("button");
+        saveButton.classList.add("btn" ,"btn-success", "mt-3");
+        saveButton.innerText = "Save Changes";
+        saveButton.addEventListener("click", async () => {
+            const updatedResponse = responseInput.value;
+            const updatedStatus = statusSelect.value;
+             sendAdminResponse(requestId, updatedResponse, updatedStatus); 
+        });
+
+        modalBody.appendChild(saveButton);
+        } else {
+            const adminResponse = document.createElement("p");
+            adminResponse.innerText = "Manager response: " + (product.adminrespons || "No manager response");
+            modalBody.appendChild(adminResponse);
+        }
+
+        const modal = new bootstrap.Modal(document.getElementById("reportModal"));
+        modal.show();
+
+}
 function createReportCard(product) {
+    const requestId = product.requestid;
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+    const userType = userData.userType;
+    
     const ulFrag = document.createDocumentFragment();
     const reportCard = document.createElement("div");
     reportCard.classList.add("reportCard");
@@ -80,44 +178,32 @@ function createReportCard(product) {
     for (const key in product) {
         let cardData;
         setCardData(key,reportCard,cardData,product)
+    }
+
+    if (userType === "user") {
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("icon-trash");
+        deleteBtn.classList.add("delete");
+
+        deleteBtn.addEventListener("click", (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            deleteRequest(requestId); 
+        });
+
+        reportCard.appendChild(deleteBtn);
+        console.log(product.adminrespons);
+        if (product.adminrespons) {
+            
+            const iconWrapper = document.createElement("div");
+            iconWrapper.classList.add("icon-bell");
+            reportCard.appendChild(iconWrapper);
+        }
         
     }
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("icon-trash");
-
-    deleteBtn.addEventListener("click", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        const requestId = reportCard.getAttribute("Report_ID");
-        deleteRequest(requestId); 
-    });
-
-    reportCard.appendChild(deleteBtn);
-
-    if (product.adminResponds) {
-        const iconWrapper = document.createElement("div");
-        iconWrapper.classList.add("icon-bell");
-        reportCard.appendChild(iconWrapper);
-    }
-    
-
     reportCard.addEventListener("click", () => {
-        const modalBody = document.getElementById("reportModalBody");
-
-        modalBody.innerHTML = "";
-
-        const description = document.createElement("p");
-        description.innerText = "Report description:" + (product.description || "No description");
-
-        const adminResponse = document.createElement("p");
-        adminResponse.innerText = "Manger responds: " + (product.adminResponds || "No manger response");
-
-        modalBody.appendChild(description);
-        modalBody.appendChild(adminResponse);
-
-        const modal = new bootstrap.Modal(document.getElementById("reportModal"));
-        modal.show();
+        setReportModel(userType,product,requestId);
     });
     
     return ulFrag;
@@ -158,9 +244,45 @@ async function getUserRequestFromServer(){
     alert("Failed to load request data: " + error.message);
   }
 }
+async function getRequestFromServer(){
+    try {
+    const response = await fetch(`http://localhost:8081/api/requests/requests`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      const err = await response.json();
+      throw new Error(err.message);
+    }
+
+    const data = await response.json();
+    if (Array.isArray(data) && data.length === 0) {
+      document.querySelector(".listContiner").innerText = "You don't have any reports yet.";
+      return;
+    }
+
+    initializeReportPage(data);
+  } catch (error) {
+    console.error("Request fetch error:", error.message);
+    alert("Failed to load request data: " + error.message);
+  }
+}
+function setHomePage(){
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+    const userType = userData.userType;
+    
+    if (userType === "admin") {
+        getRequestFromServer();
+        
+    }else{
+        getUserRequestFromServer();
+    }
+}
 window.onload = () =>{
     getPageTitle();
-    getUserRequestFromServer();
+    setHomePage();
+    
     document.querySelector(".btn-close").addEventListener("click",()=>{
         document.activeElement.blur();
     })
